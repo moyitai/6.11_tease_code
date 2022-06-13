2aeaffc82c23031cc0560d3d835c9fd320c6f9a4
moyitaii <1808687790@qq.com>
2022-06-13 14:31:45 +0800

    更改指令 抬腕亮屏

--- apps/watch/smartbox/sensors_data_opt/sport_data_info_opt/sport_info_raise_wrist.c
+++ apps/watch/smartbox/sensors_data_opt/sport_data_info_opt/sport_info_raise_wrist.c
@@ -20,6 +20,7 @@ enum {
 void sport_info_raise_wrist_attr_set(void *priv, u8 attr, u8 *data, u16 len)
 {
     printf("%s", __func__);
+    put_buf(data,len);
     u8 raise_wrist_switch = data[0];
     u8 raise_wrist_mode = data[1];
 
